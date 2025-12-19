737b640bbfc1f9828d1aea212bcba4443dc23b82
Ignacio Bisio <ibisio@gocloud.la>
2025-12-19 16:22:30 -0300

    feat(module): add security group description

--- security.tf
+++ security.tf
@@ -5,10 +5,11 @@ module "security_group_alb" {
   version = "5.3.1"
 
   name                     = "${local.common_name}-lb-${each.key}"
+  description              = lookup(each.value, "security_group_description", "Security Group managed by Terraform")
   vpc_id                   = data.aws_vpc.this[each.key].id
   use_name_prefix          = false
   ingress_with_cidr_blocks = lookup(each.value, "ingress_with_cidr_blocks", false)
-  egress_with_cidr_blocks = lookup(each.value, "egress_with_cidr_blocks", [
+  egress_with_cidr_blocks  = lookup(each.value, "egress_with_cidr_blocks", [
     {
       rule        = "all-all"
       cidr_blocks = data.aws_vpc.this[each.key].cidr_block

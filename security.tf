dcd195d75cdafcf8b042c4b78c7609bfc1a22030
Ignacio Bisio <ibisio@gocloud.la>
2026-01-07 10:45:09 -0300

    feat(module): add custom names & custom security groups rules (#11)

--- security.tf
+++ security.tf
@@ -4,16 +4,19 @@ module "security_group_alb" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
 
-  name                     = "${local.common_name}-lb-${each.key}"
+  create                   = lookup(each.value, "security_group_create", true)
+  name                     = lookup(each.value, "security_group_name", "${local.common_name}-lb-${each.key}")
+  description              = lookup(each.value, "security_group_description", "Security Group managed by Terraform")
   vpc_id                   = data.aws_vpc.this[each.key].id
   use_name_prefix          = false
-  ingress_with_cidr_blocks = lookup(each.value, "ingress_with_cidr_blocks", false)
+  ingress_with_cidr_blocks = lookup(each.value, "ingress_with_cidr_blocks", [])
   egress_with_cidr_blocks = lookup(each.value, "egress_with_cidr_blocks", [
     {
       rule        = "all-all"
       cidr_blocks = data.aws_vpc.this[each.key].cidr_block
     }
   ])
+  egress_with_ipv6_cidr_blocks = lookup(each.value, "egress_with_ipv6_cidr_blocks", [])
 
   tags = local.common_tags
 }
